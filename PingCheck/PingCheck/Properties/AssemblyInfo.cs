60ae3ee216f9ea6198ca94a5a5e8e6db3de7540d
Phi Bui <phibangbui.cs@gmail.com>
2014-10-31 10:47:02 -0700

    changed application title and getting ready for public release

--- PingCheck/PingCheck/Properties/AssemblyInfo.cs
+++ PingCheck/PingCheck/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication1")]
+[assembly: AssemblyTitle("Ping Check!")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication1")]
+[assembly: AssemblyProduct("Ping Check!")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
